ee842524f2a6e8dea25b91357b39ca3cddc275ed
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-01-28 14:33:11 +0300

    add action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import getDiff from '../src/compare-json-files.js';
+import generateDiff from '../src/compare-json-files.js';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference')
-    .option('-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
-    .argument('<path1>', 'the first path to compare')
-    .argument('<path2>', 'the second path to compare')
-    .action((path1, path2) => {
-        getDiff(path1, path2)
-    });
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<path1>', 'the first path to compare')
+  .argument('<path2>', 'the second path to compare')
+  .action((path1, path2) => {
+    generateDiff(path1, path2);
+  });
 program.parse();
