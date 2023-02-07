94ac4dc286da9c8f533320d1d16b3ab4fbca0ed7
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-07 15:12:21 +0300

    add test on plain view

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,8 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('get difference between nested structure', () => {
-  const content = fs.readFileSync(getFixturePath('difference.txt'), 'utf-8');
+test('diff between two nested structure', () => {
+  const content = fs.readFileSync(getFixturePath('tree-view-of-difference.txt'), 'utf-8');
   expect(
     runDiff(getFixturePath('file1.json'), getFixturePath('file2.json')),
   ).toEqual(content);
@@ -31,4 +31,14 @@ test('get difference between nested structure', () => {
       getFixturePath('file2.json'),
     );
   }).toThrow("File doesn't exist");
+});
+
+test('diff in plain format', () => {
+  const content = fs.readFileSync(getFixturePath('flat-difference.txt'), 'utf-8');
+  expect(
+    runDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain'),
+  ).toEqual(content);
+  expect(
+    runDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain'),
+  ).toEqual(content);
 });
