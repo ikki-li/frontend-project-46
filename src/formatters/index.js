3fb0eafdc9b5c618b2be9f4ee2e566ea70ea4fe3
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-08 15:19:39 +0300

    add processed argument value

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,13 @@
 import { generateTreeView } from './stylish.js';
 import { generatePlainView } from './plain.js';
+import { generateJsonView } from './json.js';
 
 const chooseFormat = (formatName) => {
   if (formatName === 'plain') {
     return generatePlainView;
+  }
+  if (formatName === 'json') {
+    return generateJsonView;
   }
   return generateTreeView;
 };
