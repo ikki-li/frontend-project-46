093618b666c368c3d31ebe0d021b940de404c260
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-10 12:37:42 +0300

    optimize function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,27 +1,77 @@
+import _ from 'lodash';
+
+const indent = ' ';
+const baseSpaceCount = 4;
+const signSpaceCount = 2;
+
+const formatObject = (object, depth) => {
+  const indentSize = baseSpaceCount * (depth + 1);
+  const curIndent = indent.repeat(indentSize);
+  const bracketIndentSize = baseSpaceCount * depth;
+  const bracketIndent = indent.repeat(bracketIndentSize);
+  if (Array.isArray(object)) {
+    const itemsView = object.map((item) => {
+      if (_.isObject(item)) {
+        return formatObject(item, depth + 1);
+      }
+      return `${curIndent}${item}`;
+    });
+    return ['[', ...itemsView, `${bracketIndent}]`].join('\n');
+  }
+  const keys = Object.keys(object);
+  const lines = keys.map((key) => {
+    const value = object[key];
+    const formattedValue = _.isObject(value) ? formatObject(value, depth + 1) : value;
+    return `${curIndent}${key}: ${formattedValue}`;
+  });
+  return ['{', ...lines, `${bracketIndent}}`].join('\n');
+};
+
 const generateTreeView = (data) => {
-  const indent = ' ';
-  const baseSpaceCount = 4;
-  const signSpaceCount = 2;
   const iter = (currentItem, depth) => {
     const currentIndentSize = (baseSpaceCount * depth) - signSpaceCount;
     const currentIndent = indent.repeat(currentIndentSize);
     const bracketIndentSize = (baseSpaceCount * depth) - baseSpaceCount;
     const bracketIndent = indent.repeat(bracketIndentSize);
-    const lines = currentItem.flatMap(([sign, key, value]) => {
-      if (!Array.isArray(value)) {
-        return `${currentIndent}${sign} ${key}: ${value}`;
+    const lines = currentItem.map((node) => {
+      const { name, type } = node;
+      switch (type) {
+        case 'node': {
+          const { children } = node;
+          const childrenView = iter(children, depth + 1);
+          return `${currentIndent}  ${name}: ${childrenView}`;
+        }
+        case 'changed': {
+          const { value1, value2 } = node;
+          const formattedValue1 = _.isObject(value1)
+            ? formatObject(value1, depth) : value1;
+          const formattedValue2 = _.isObject(value2)
+            ? formatObject(value2, depth) : value2;
+          return `${currentIndent}- ${name}: ${formattedValue1}\n${currentIndent}+ ${name}: ${formattedValue2}`;
+        }
+        case 'added': {
+          const { value } = node;
+          const formattedValue = _.isObject(value)
+            ? formatObject(value, depth) : value;
+          return `${currentIndent}+ ${name}: ${formattedValue}`;
+        }
+        case 'deleted': {
+          const { value } = node;
+          const formattedValue = _.isObject(value)
+            ? formatObject(value, depth) : value;
+          return `${currentIndent}- ${name}: ${formattedValue}`;
+        }
+        case 'unchanged': {
+          const { value } = node;
+          const formattedValue = _.isObject(value)
+            ? formatObject(value, depth)
+            : value;
+          return `${currentIndent}  ${name}: ${formattedValue}`;
+        }
+        default: {
+          throw new Error('Type of node is not defined');
+        }
       }
-      if (value[0] === 'array') {
-        const [, items] = value;
-        const indentSizeOfItem = baseSpaceCount * depth + baseSpaceCount;
-        const indentOfItem = indent.repeat(indentSizeOfItem);
-        const bracketIndentSizeItem = currentIndentSize + signSpaceCount;
-        const bracketIndentItem = indent.repeat(bracketIndentSizeItem);
-        const formattedItems = items.map((item) => `${indentOfItem}${item}`);
-        const formattedValue = ['[', ...formattedItems, `${bracketIndentItem}]`].join('\n');
-        return `${currentIndent}${sign} ${key}: ${formattedValue}`;
-      }
-      return `${currentIndent}${sign} ${key}: ${iter(value, depth + 1)}`;
     });
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
   };
