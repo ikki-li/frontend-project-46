0c2311e3306f4cf755b1578d4c5a53f6020635c5
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-12 11:25:50 +0300

    optimize call chain

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,13 +5,12 @@ import genDiff from '../src/index.js';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
-  .option('-V, --version', 'output the version number')
+  .option('-V, --version')
   .argument('<filepath1>', 'the first path to compare')
   .argument('<filepath2>', 'the second path to compare')
   .option('-f, --format <type>', 'format difference', 'stylish')
   .action((filepath1, filepath2, options) => {
     const formatName = options.format;
     genDiff(filepath1, filepath2, formatName);
-  });
-
-program.parse();
+  })
+  .parse();
