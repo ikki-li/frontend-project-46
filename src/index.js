d65ef4f1855364709a440e54cbd52285b3c384e0
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-13 10:28:44 +0300

    decompose main function

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 import path from 'path';
 import fs from 'node:fs';
 import parse from './parsers/parser.js';
-import chooseFormat from './formatters/index.js';
+import format from './formatters/index.js';
 import compare from './comparators/compare.js';
 
+const normalizePath = (filepath) => path.resolve(filepath);
+const getFormat = (filepath) => {
+  if (!fs.existsSync(filepath)) {
+    throw new Error(`"${path.basename(filepath)} doesn't exist"`);
+  }
+  return path.extname(filepath);
+};
+const getData = (filepath) => {
+  const normalizedPath = normalizePath(filepath);
+  const fileFormat = getFormat(normalizedPath);
+  const data = fs.readFileSync(normalizedPath, 'utf-8');
+  if (data.length === 0) {
+    throw new Error(`${path.basename(normalizedPath)} is empty`);
+  }
+  return parse(data, fileFormat);
+};
+
 const genDiff = (path1, path2, formatName = 'stylish') => {
-  const normalizedPath1 = path.resolve(path1);
-  const normalizedPath2 = path.resolve(path2);
-  if (!fs.existsSync(normalizedPath1) || !fs.existsSync(normalizedPath2)) {
-    throw new Error("File doesn't exist");
-  }
-  const format1 = path.extname(normalizedPath1);
-  const format2 = path.extname(normalizedPath2);
-  const data1 = fs.readFileSync(normalizedPath1, 'utf-8');
-  const data2 = fs.readFileSync(normalizedPath2, 'utf-8');
-  if (data1.length === 0 && data2.length === 0) {
-    throw new Error('Files are empty');
-  }
-  if (data1.length === 0) {
-    throw new Error(`${path.basename(normalizedPath1)} is empty`);
-  }
-  if (data2.length === 0) {
-    throw new Error(`${path.basename(normalizedPath2)} is empty`);
-  }
-  const object1 = parse(data1, format1);
-  const object2 = parse(data2, format2);
-  const difference = compare(object1, object2);
-  const format = chooseFormat(formatName);
-  console.log(format(difference));
-  return format(difference);
+  const data1 = getData(path1);
+  const data2 = getData(path2);
+  const difference = compare(data1, data2);
+  return format(formatName, difference);
 };
 
 export default genDiff;
