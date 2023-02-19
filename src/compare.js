0da7407555d0d7ddd247b67dad0e4c801cf198d3
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-19 13:13:34 +0300

    optimize internal logic

--- src/compare.js
+++ src/compare.js
@@ -3,61 +3,65 @@ import _ from 'lodash';
 const compare = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-  const commonKeys = _.union(keys1, keys2);
-  const sortedCommonKeys = _.sortBy(commonKeys);
-  const difference = sortedCommonKeys.map((key) => {
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+  return sortedKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      const value2 = _.isObject(data2[key])
+        ? _.cloneDeep(data2[key])
+        : data2[key];
+      return {
+        key,
+        type: 'added',
+        value: value2,
+      };
+    }
+
+    if (!_.has(data2, key)) {
+      const value1 = _.isObject(data1[key])
+        ? _.cloneDeep(data1[key])
+        : data1[key];
+      return {
+        key,
+        type: 'deleted',
+        value: value1,
+      };
+    }
+
+    if (
+      _.isPlainObject(data1[key])
+            && _.isPlainObject(data2[key])
+    ) {
+      const children = compare(data1[key], data2[key]);
+      return {
+        key,
+        type: 'nested',
+        children,
+      };
+    }
+
+    if (_.isEqual(data1[key], data2[key])) {
+      const value = _.isObject(data1[key])
+        ? _.cloneDeep(data1[key])
+        : data1[key];
+      return {
+        key,
+        type: 'unchanged',
+        value,
+      };
+    }
     const value1 = _.isObject(data1[key])
       ? _.cloneDeep(data1[key])
       : data1[key];
     const value2 = _.isObject(data2[key])
       ? _.cloneDeep(data2[key])
       : data2[key];
-
-    if (
-      _.isObject(value1)
-            && !Array.isArray(value1)
-            && _.isObject(value2)
-            && !Array.isArray(value2)
-    ) {
-      const children = compare(value1, value2);
-      return {
-        name: key,
-        type: 'nested',
-        children,
-      };
-    }
-    if (!Object.hasOwn(data1, key)) {
-      return {
-        name: key,
-        type: 'plained',
-        status: 'added',
-        values: [value2],
-      };
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return {
-        name: key,
-        type: 'plained',
-        status: 'deleted',
-        values: [value1],
-      };
-    }
-    if (_.isEqual(value1, value2)) {
-      return {
-        name: key,
-        type: 'plained',
-        status: 'unchanged',
-        values: [value1],
-      };
-    }
     return {
-      name: key,
-      type: 'plained',
-      status: 'changed',
-      values: [value1, value2],
+      key,
+      type: 'changed',
+      value1,
+      value2,
     };
   });
-  return difference;
 };
 
 export default compare;
