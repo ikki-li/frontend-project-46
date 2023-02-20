548f3489e98fd9d859265db176de589fabaabedd
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-20 12:35:39 +0300

    optimize internal logic

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,35 +4,41 @@ const indent = ' ';
 const baseSpaceCount = 4;
 const signSpaceCount = 2;
 
-const formatObject = (object, depth) => {
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
   const indentSize = baseSpaceCount * (depth + 1);
   const curIndent = indent.repeat(indentSize);
   const bracketIndentSize = baseSpaceCount * depth;
   const bracketIndent = indent.repeat(bracketIndentSize);
-  if (Array.isArray(object)) {
-    const itemsView = object.map((item) => {
+  if (Array.isArray(value)) {
+    const itemsView = value.map((item) => {
       if (_.isObject(item)) {
-        return formatObject(item, depth + 1);
+        return stringify(item, depth + 1);
       }
       return `${curIndent}${item}`;
     });
     return ['[', ...itemsView, `${bracketIndent}]`].join('\n');
   }
-  const keys = Object.keys(object);
-  const lines = keys.map((key) => {
-    const value = object[key];
-    const formattedValue = _.isObject(value) ? formatObject(value, depth + 1) : value;
-    return `${curIndent}${key}: ${formattedValue}`;
-  });
-  return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  const lines = Object
+    .entries(value)
+    .map(([key, val]) => `${curIndent}${key}: ${stringify(val, depth + 1)}`);
+
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
 };
 
 const generateStylishView = (data) => {
   const iter = (currentItem, depth) => {
-    const currentIndentSize = (baseSpaceCount * depth) - signSpaceCount;
+    const currentIndentSize = baseSpaceCount * depth - signSpaceCount;
     const currentIndent = indent.repeat(currentIndentSize);
-    const bracketIndentSize = (baseSpaceCount * depth) - baseSpaceCount;
+    const bracketIndentSize = baseSpaceCount * depth - baseSpaceCount;
     const bracketIndent = indent.repeat(bracketIndentSize);
+
     const lines = currentItem.map((node) => {
       const { key, type } = node;
       switch (type) {
@@ -43,33 +49,23 @@ const generateStylishView = (data) => {
         }
         case 'changed': {
           const { value1, value2 } = node;
-          const formattedValue1 = _.isObject(value1)
-            ? formatObject(value1, depth)
-            : value1;
-          const formattedValue2 = _.isObject(value2)
-            ? formatObject(value2, depth)
-            : value2;
+          const formattedValue1 = stringify(value1, depth);
+          const formattedValue2 = stringify(value2, depth);
           return `${currentIndent}- ${key}: ${formattedValue1}\n${currentIndent}+ ${key}: ${formattedValue2}`;
         }
         case 'added': {
           const { value } = node;
-          const formattedValue = _.isObject(value)
-            ? formatObject(value, depth)
-            : value;
+          const formattedValue = stringify(value, depth);
           return `${currentIndent}+ ${key}: ${formattedValue}`;
         }
         case 'deleted': {
           const { value } = node;
-          const formattedValue = _.isObject(value)
-            ? formatObject(value, depth)
-            : value;
+          const formattedValue = stringify(value, depth);
           return `${currentIndent}- ${key}: ${formattedValue}`;
         }
         case 'unchanged': {
           const { value } = node;
-          const formattedValue = _.isObject(value)
-            ? formatObject(value, depth)
-            : value;
+          const formattedValue = stringify(value, depth);
           return `${currentIndent}  ${key}: ${formattedValue}`;
         }
         default: {
