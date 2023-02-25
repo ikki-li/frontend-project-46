e33a2f8635c539d6dd39be3a09d614d7c18d8abe
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-25 16:20:03 +0300

    remove excess variables

--- src/compare.js
+++ src/compare.js
@@ -6,61 +6,20 @@ const compare = (data1, data2) => {
   const sortedKeys = _.sortBy(_.union(keys1, keys2));
   return sortedKeys.map((key) => {
     if (!_.has(data1, key)) {
-      const value2 = _.isObject(data2[key])
-        ? _.cloneDeep(data2[key])
-        : data2[key];
-      return {
-        key,
-        type: 'added',
-        value: value2,
-      };
+      return { key, type: 'added', value: data2[key] };
     }
 
     if (!_.has(data2, key)) {
-      const value1 = _.isObject(data1[key])
-        ? _.cloneDeep(data1[key])
-        : data1[key];
-      return {
-        key,
-        type: 'deleted',
-        value: value1,
-      };
+      return { key, type: 'deleted', value: data1[key] };
     }
-
-    if (
-      _.isPlainObject(data1[key])
-            && _.isPlainObject(data2[key])
-    ) {
-      const children = compare(data1[key], data2[key]);
-      return {
-        key,
-        type: 'nested',
-        children,
-      };
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, type: 'nested', children: compare(data1[key], data2[key]) };
     }
 
     if (_.isEqual(data1[key], data2[key])) {
-      const value = _.isObject(data1[key])
-        ? _.cloneDeep(data1[key])
-        : data1[key];
-      return {
-        key,
-        type: 'unchanged',
-        value,
-      };
+      return { key, type: 'unchanged', value: data1[key] };
     }
-    const value1 = _.isObject(data1[key])
-      ? _.cloneDeep(data1[key])
-      : data1[key];
-    const value2 = _.isObject(data2[key])
-      ? _.cloneDeep(data2[key])
-      : data2[key];
-    return {
-      key,
-      type: 'changed',
-      value1,
-      value2,
-    };
+    return { key, type: 'changed', value1: data1[key], value2: data2[key] };
   });
 };
 
