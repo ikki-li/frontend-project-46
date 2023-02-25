1b21de0f2c091159c7e314a3d0c819442ad52dbe
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2023-02-25 16:27:19 +0300

    remove excess variables

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,40 +9,35 @@ const formatValue = (value) => {
   }
 };
 
-const generatePlainView = (data) => {
-  const iter = (currentItem, path) => {
-    const lines = currentItem.flatMap((node) => {
-      const { key, type } = node;
-      const newPath = `${path}${key}.`;
-      switch (type) {
-        case 'nested': {
-          const { children } = node;
-          return iter(children, newPath);
-        }
-        case 'changed': {
-          const { value1, value2 } = node;
-          const formattedValue1 = formatValue(value1);
-          const formattedValue2 = formatValue(value2);
-          return `Property '${path}${key}' was updated. From ${formattedValue1} to ${formattedValue2}`;
-        }
-        case 'added': {
-          const { value } = node;
-          const formattedValue = formatValue(value);
-          return `Property '${path}${key}' was added with value: ${formattedValue}`;
-        }
-        case 'deleted':
-          return `Property '${path}${key}' was removed`;
-        case 'unchanged': {
-          return '';
-        }
-        default:
-          throw new Error(`Node type ${type} is not defined`);
+const iter = (tree, path) => {
+  const lines = tree.flatMap((node) => {
+    const { key, type } = node;
+    const newPath = `${path}${key}.`;
+    switch (type) {
+      case 'nested': {
+        const { children } = node;
+        return iter(children, newPath);
       }
-    }, []);
-    const filteredLines = lines.filter((child) => child.length !== 0);
-    return [...filteredLines].join('\n');
-  };
-  return iter(data, '');
+      case 'changed': {
+        const { value1, value2 } = node;
+        return `Property '${path}${key}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
+      }
+      case 'added': {
+        const { value } = node;
+        return `Property '${path}${key}' was added with value: ${formatValue(value)}`;
+      }
+      case 'deleted':
+        return `Property '${path}${key}' was removed`;
+      case 'unchanged': {
+        return '';
+      }
+      default:
+        throw new Error(`Node type ${type} is not defined`);
+    }
+  }, []);
+  return lines.filter((child) => child.length !== 0);
 };
 
-export default generatePlainView;
+const formatPlain = (data) => iter(data, '').join('\n');
+
+export default formatPlain;
